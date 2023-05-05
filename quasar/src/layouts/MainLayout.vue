ae3c6c00a41973ff2a858d0182f23ada0af05f73
Sergio Alferov <alferovsergio@yandex.ru>
2023-05-05 12:11:56 +0300

    quasar-app

---
+++ quasar/src/layouts/MainLayout.vue
@@ -0,0 +1,104 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar.png" />
+          </q-avatar>
+          Kate Miller
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <div class="q-pa-md q-gutter-sm" style="height: 80px">
+        <q-avatar
+          v-for="n in 5"
+          :key="n"
+          size="40px"
+          class="overlapping"
+          :style="`left: ${n * 25}px`"
+        >
+          <img :src="`https://cdn.quasar.dev/img/avatar${n + 1}.jpg`" />
+        </q-avatar>
+      </div>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+      <div class="q-pa-md" style="max-width: 400px">
+        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+          <q-input
+            filled
+            v-model="name"
+            label="Your name *"
+            hint="Name and surname"
+            lazy-rules
+            :rules="[
+              (val) => (val && val.length > 0) || 'Please type something',
+            ]"
+          />
+
+          <q-input
+            filled
+            type="number"
+            v-model="age"
+            label="Your age *"
+            lazy-rules
+            :rules="[
+              (val) => (val !== null && val !== '') || 'Please type your age',
+              (val) => (val > 0 && val < 100) || 'Please type a real age',
+            ]"
+          />
+
+          <q-toggle v-model="accept" label="I accept the license and terms" />
+
+          <div>
+            <q-btn label="Submit" type="submit" color="primary" />
+            <q-btn
+              label="Reset"
+              type="reset"
+              color="primary"
+              flat
+              class="q-ml-sm"
+            />
+          </div>
+        </q-form>
+      </div>
+      <q-card class="my-card">
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Title</q-item-label>
+            <q-item-label caption>Subhead</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      </q-card>
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div>Footer</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  setup() {
+    return {};
+  },
+});
+</script>
